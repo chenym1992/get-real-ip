cec9d5a7c70c6b7ab921aa07e1bcecc1929fe435
chenym1992 <18251821296@163.com>
2023-08-04 15:42:36 +0800

    chore: close on complete

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 const config = {
-    iceServers: [
-        {
-            urls: ['stun:stun.l.google.com:19302']
-        },
-    ]
+  iceServers: [
+    {
+      urls: ["stun:stun.l.google.com:19302"],
+    },
+  ],
 };
 /**
  * get real ip by RTCPeerConnection
  * @params iceServers
- * @returns 
+ * @returns
  */
 export default function getRealIp(iceServers) {
-    return new Promise((resolve) => {
-        if (iceServers.length) {
-            config.iceServers = iceServers
-        }
-        const pc = new RTCPeerConnection(config);
-        pc.onicecandidate = e => {
-            if (e?.candidate?.type === "srflx") {
-                resolve(e.candidate.address)
-            }
-        }
-        pc.createOffer({ offerToReceiveAudio: 1 }).then(e => {
-            pc.setLocalDescription(e)
-        });
-    })
+  return new Promise((resolve) => {
+    if (iceServers.length) {
+      config.iceServers = iceServers;
+    }
+    let pc = new RTCPeerConnection(config);
+    pc.onicecandidate = (e) => {
+      if (e?.candidate?.type === "srflx") {
+        resolve(e.candidate.address);
+      }
+    };
+    pc.onicegatheringstatechange = () => {
+      if (pc.iceGatheringState === "complete") {
+        pc.close();
+        pc = null;
+      }
+    };
+    pc.createOffer({ offerToReceiveAudio: 1 }).then((e) => {
+      pc.setLocalDescription(e);
+    });
+  });
 }
