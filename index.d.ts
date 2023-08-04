f8a7c2f45f541a0289de99557e02db44f1ad12ff
chenym1992 <18251821296@163.com>
2023-08-04 16:00:53 +0800

    chore: types update

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+declare module "getRealIp" {
+  /**
+   * Get the real IP address using RTCPeerConnection.
+   * @param iceServers - The array of ICE servers.
+   * @returns A promise that resolves to the real IP address.
+   */
+  export default function getRealIp(
+    iceServers?: RTCIceServer[]
+  ): Promise<string>;
+}
